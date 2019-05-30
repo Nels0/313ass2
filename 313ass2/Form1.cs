432fa711e0303bdd21114951e99f5295f64cf920
lizzae <38896161+lizzae@users.noreply.github.com>
2019-05-30 14:59:25 +1200

    lab progress II (It works)

--- 313ass2/Form1.cs
+++ 313ass2/Form1.cs
@@ -11,25 +11,21 @@ using System.Threading;
 
 namespace _313ass2
 {
+
     public partial class Form1 : Form
     {
-        static int toolRegister = 0;
-        static Tool fan = new Tool(0);
-        static Tool heater = new Tool(1);
-        static DigitalO dOut = new DigitalO();
-
         
+
 
         public Form1()
         {
             InitializeComponent();
 
-            //string device = "dev6";
+            
 
-            //Chamber chamber1 = new Chamber();
+            Chamber chamber1 = new Chamber();
 
-           // Tool fan = new Tool(0);
-            //Tool heater = new Tool(1);
+
 
             //dOut.OpenChannel(device+"/port0", "DigitalChn0");
             //aIn0.OpenChannel("dev6/ai0", "Ainput");
@@ -38,51 +34,7 @@ namespace _313ass2
 
         }
 
-        class Chamber
-        {
-
-            double readTemp;
-            double setPoint;
-            double ambientTemp;
-            bool controllerEnabled;
-            int kp;
-
-            public Chamber()
-            {
-                kp = 1;
-            }
-
-            public void Controller()
-            {
-                //Controller calculations
-
-                double error = setPoint - readTemp;
-
-                double y = error * (double)kp;
-
-                //Convert to output and
-                //Only apply output if controller is "started"
-                if (controllerEnabled)
-                {
-                    if (y > 0)
-                    {
-                        heater.on = true;
-                        fan.on = false;
-                    }
-                    else
-                    {
-                        heater.on = false;
-                        fan.on = true;
-                    }
-                }
-                else
-                {
-                    heater.on = false;
-                    fan.on = false;
-                }
-            }
-        }
-
+        
 
         
 
@@ -92,43 +44,7 @@ namespace _313ass2
         }
 
 
-        class Tool
-        {
-            private bool _on;
-            protected int toolID;
-            public bool on
-            {
-                get { return _on; }
-                set
-                {
-                    _on = value;
-                    commitState();
-                }
-            }
-
-            public Tool(int ID)
-            {
-                on = false;
-                toolID = ID;
-            }
-
-            private void commitState()
-            {
-
-                if (_on)
-                {
-                    toolRegister |= (1 << toolID);
-                }
-                else
-                {
-                    toolRegister &= ~(1 << toolID);
-                }
-
-                dOut.WriteData(toolRegister);
-            }
-
-        }
-
+        
         private void fileToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
@@ -144,4 +60,98 @@ namespace _313ass2
 
         }
     }
+
+    class Tool
+    {
+        static DigitalO dOut = new DigitalO();
+        static int toolRegister = 0;
+
+        private bool _on;
+        protected int toolID;
+        public bool on
+        {
+            get { return _on; }
+            set
+            {
+                _on = value;
+                commitState();
+            }
+        }
+
+        public Tool(int ID)
+        {
+            string device = "dev6";
+
+            on = false;
+            toolID = ID;
+            //dOut.OpenChannel(device + "/port0", "DigitalChn0");
+        }
+
+        private void commitState()
+        {
+
+            if (_on)
+            {
+                toolRegister |= (1 << toolID);
+            }
+            else
+            {
+                toolRegister &= ~(1 << toolID);
+            }
+
+            dOut.WriteData(toolRegister);
+        }
+
+    }
+
+    class Chamber
+    {
+        Tool fan = new Tool(0);
+        Tool heater = new Tool(1);
+
+        double readTemp;
+        double setPoint;
+        double ambientTemp;
+        bool controllerEnabled;
+        int kp;
+
+
+
+        public Chamber()
+        {
+            kp = 1;
+        }
+
+        public void Controller()
+        {
+            //Controller calculations
+
+            double error = setPoint - readTemp;
+
+            double y = error * (double)kp;
+
+            //Convert to output and
+            //Only apply output if controller is "started"
+            if (controllerEnabled)
+            {
+                if (y > 0)
+                {
+                    heater.on = true;
+                    fan.on = false;
+                }
+                else
+                {
+                    heater.on = false;
+                    fan.on = true;
+                }
+            }
+            else
+            {
+                heater.on = false;
+                fan.on = false;
+            }
+        }
+    }
+
+
 }
--- 313ass2/Program.cs
+++ 313ass2/Program.cs
@@ -15,7 +15,8 @@ namespace _313ass2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Form1 form = new Form1();
+            Application.Run(form);
 
 
         }
